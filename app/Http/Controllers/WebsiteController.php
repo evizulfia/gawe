dd6e52c9cbdda6b988e040484ff5a18fae2164c4
Pandu Sunarya <pandusnrya@gmail.com>
2024-06-05 20:40:36 +0700

    setup mail function

--- app/Http/Controllers/WebsiteController.php
+++ app/Http/Controllers/WebsiteController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Mail\Websitemail;
+use Hash;
+use Auth;
 
 class WebsiteController extends Controller
 {
@@ -24,6 +28,34 @@ class WebsiteController extends Controller
     public function registration()
     {
         return view('registration');
+    }
+
+    public function registration_submit(Request $request)
+    {
+
+        $token = hash('sha256',time());
+        
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->status = 'Pending';
+        $user->token = $token;
+        $user->save();
+
+        $verification_link = url('registration/verify/'.$token.'/'.$request->email);
+        $subject = 'Registration Confirmation';
+        $message = 'Please click on this link: <br><a href="'.$verification_link.'">Click here</a>';
+
+        \Mail::to($request->email)->send(new Websitemail($subject,$message));
+
+        echo 'Email is sent';
+
+    }
+
+    public function registration_verify()
+    {
+
     }
 
     public function forget_password()
