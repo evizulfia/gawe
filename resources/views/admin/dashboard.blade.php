925311269673ddea18a6697538620888809f0710
Pandu Sunarya <pandusnrya@gmail.com>
2024-06-06 08:02:54 +0700

    fix user login routes

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,4 +1,4 @@
 @include('admin.nav')
-
+@csrf
 <h3>Dashboard - Admin</h3>
 <p>Hi {{ Auth::guard('admin')->user()->name }}, Welcome to dashboard!</p>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,4 @@
 @include('nav')
-
+@csrf
 <h3>Dashboard - User</h3>
 <p>Hi {{ Auth::guard('web')->user()->name }}, Welcome to dashboard!</p>
