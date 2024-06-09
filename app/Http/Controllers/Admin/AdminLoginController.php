f0edc906fe40505f7d9d02d33b2fdaa1631c867f
Pandu Sunarya <pandusnrya@gmail.com>
2024-06-09 20:44:20 +0700

    forget password + sending email reset link

--- app/Http/Controllers/Admin/AdminLoginController.php
+++ app/Http/Controllers/Admin/AdminLoginController.php
@@ -20,4 +20,30 @@ class AdminLoginController extends Controller
     {
         return view('admin.forget_password');
     }
+
+    public function login_submit(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+
+        $credential = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+
+        if(Auth::guard('admin')->attempt($credential)) {
+            return redirect()->route('admin_home');
+        } else {
+            return redirect()->route('admin_login')->with('error', 'Information is not correct!');
+        }
+    }
+
+    public function logout()
+    {
+        Auth::guard('admin')->logout();
+        return redirect()->route('admin_login');
+    }
 }
