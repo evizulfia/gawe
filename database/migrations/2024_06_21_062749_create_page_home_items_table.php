df220dce325f459fba120d0f928e3b03e306df71
Pandu Sunarya <pandusnrya@gmail.com>
2024-06-25 00:37:46 +0700

    Update 2024_06_21_062749_create_page_home_items_table.php

--- database/migrations/2024_06_21_062749_create_page_home_items_table.php
+++ database/migrations/2024_06_21_062749_create_page_home_items_table.php
@@ -21,7 +21,7 @@ class CreatePageHomeItemsTable extends Migration
             $table->text('job_category');
             $table->text('job_location');
             $table->text('search');
-            $table->text('backgroud');
+            $table->text('background');
             $table->timestamps();
         });
     }
