29b60f390b630cb67ebea5efac0e840e340e86f7
Evi Zulfia Rahmah <evizulfia@gmail.com>
2024-06-25 15:10:15 +0700

    fix

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -22,4 +22,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitc61ff82cbf0142a401a48a8161e1595a::getLoader();
+return ComposerAutoloaderInit93b65121b6412082e9c823c8adef1fbd::getLoader();
