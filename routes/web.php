977a1fa657f6254ed3642ad2278faf807ddecced
Evi Zulfia Rahmah <evizulfia@gmail.com>
2024-06-26 01:33:09 +0700

    Revert "admin routes adjusments"
    This reverts commit b82d7054a93e760ebf3e2a541709c124adfee3b0.

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,6 @@ use App\Http\Controllers\WebsiteController;
 use App\Http\Controllers\Front\TermsController;
 use App\Http\Controllers\Front\HomeController;
 use App\Http\Controllers\Admin\AdminHomeController;
-use App\Http\Controllers\Admin\AdminHomePageController;
 use App\Http\Controllers\Admin\AdminLoginController;
 use App\Http\Controllers\Admin\AdminProfileController;
 Route::get('/', [HomeController::class, 'index'])->name('home');
@@ -32,7 +31,7 @@ Route::get('/reset-password/{token}/{email}', [WebsiteController::class, 'reset_
 Route::post('/reset_password_submit', [WebsiteController::class, 'reset_password_submit'])->name('reset_password_submit');
 
 /* Admin */
-
+Route::get('/admin/home', [AdminHomeController::class, 'index'])->name('admin_home')->middleware('admin:admin');
 Route::get('/admin/login', [AdminLoginController::class, 'index'])->name('admin_login');
 Route::post('/admin/login-submit', [AdminLoginController::class, 'login_submit'])->name('admin_login_submit');
 Route::get('/admin/logout', [AdminLoginController::class, 'logout'])->name('admin_logout');
@@ -41,9 +40,5 @@ Route::post('/admin/forget-password-submit', [AdminLoginController::class, 'forg
 Route::get('/admin/reset-password/{token}/{email}', [AdminLoginController::class, 'reset_password'])->name('admin_reset_password');
 Route::post('/admin/reset-password-submit', [AdminLoginController::class, 'reset_password_submit'])->name('admin_reset_password_submit');
 
-Route::middleware('admin:admin')->group(function (){
-    Route::get('/admin/home', [AdminHomeController::class, 'index'])->name('admin_home');
-    Route::get('/admin/edit-profile', [AdminProfileController::class, 'index'])->name('admin_profile');
-    Route::post('/admin/edit-profile-submit', [AdminProfileController::class, 'profile_submit'])->name('admin_profile_submit');
-    Route::get('/admin/home-page', [AdminHomePageController::class, 'index'])->name('admin_home_page');
-});
+Route::get('/admin/edit-profile', [AdminProfileController::class, 'index'])->name('admin_profile')->middleware('admin:admin');
+Route::post('/admin/edit-profile-submit', [AdminProfileController::class, 'profile_submit'])->name('admin_profile_submit');
