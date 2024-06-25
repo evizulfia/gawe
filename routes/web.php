7ec2a5f15dd29572b0a7ddd7cb42d773a646b960
Evi Zulfia Rahmah <evizulfia@gmail.com>
2024-06-25 17:26:08 +0700

    fix routes

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,28 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WebsiteController;
 use App\Http\Controllers\Admin\AdminHomeController;
 use App\Http\Controllers\Admin\AdminLoginController;
 use App\Http\Controllers\Admin\AdminProfileController;
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [WebsiteController::class, 'index'])->name('home');
+Route::get('/dashboard', [WebsiteController::class, 'dashboard'])->name('dashboard')->middleware('auth');
+
+Route::get('/login', [WebsiteController::class, 'login'])->name('login');
+Route::post('/login-submit', [WebsiteController::class, 'login_submit'])->name('login_submit');
+
+Route::get('/logout', [WebsiteController::class, 'logout'])->name('logout');
+
+Route::get('/registration', [WebsiteController::class, 'registration'])->name('registration');
+Route::post('/registration_submit', [WebsiteController::class, 'registration_submit'])->name('registration_submit');
+
+Route::get('/registration/verify/{token}/{email}', [WebsiteController::class, 'registration_verify']);
+
+Route::get('/forget-password', [WebsiteController::class, 'forget_password'])->name('forget_password');
+Route::post('/forget_password_submit', [WebsiteController::class, 'forget_password_submit'])->name('forget_password_submit');
+
+Route::get('/reset-password/{token}/{email}', [WebsiteController::class, 'reset_password'])->name('reset_password');
+Route::post('/reset_password_submit', [WebsiteController::class, 'reset_password_submit'])->name('reset_password_submit');
 
 /* Admin */
 Route::get('/admin/home', [AdminHomeController::class, 'index'])->name('admin_home')->middleware('admin:admin');
