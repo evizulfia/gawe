2f356b92b990eb72ff3762812a3e5581ad4001a9
Evi Zulfia Rahmah <evizulfia@gmail.com>
2024-06-27 00:02:28 +0700

    fix faq page

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -4,10 +4,14 @@ namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\PageHomeItem;
 
 class HomeController extends Controller
 {
     public function index() {
+
+        $home_page_data = PageHomeItem::where('id',1)->first();
+        
         return view('front.home');
     }
 }
