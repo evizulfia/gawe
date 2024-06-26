85a48d3f1e0e607cb6652df3cb11e04d7af48a5b
Evi Zulfia Rahmah <evizulfia@gmail.com>
2024-06-26 23:29:23 +0700

    contact page creation

--- routes/web.php
+++ routes/web.php
@@ -10,12 +10,15 @@ use App\Http\Controllers\Admin\AdminHomePageController;
 use App\Http\Controllers\Admin\AdminLoginController;
 use App\Http\Controllers\Admin\AdminProfileController;
 use App\Http\Controllers\Admin\AdminJobCategoryController;
+use App\Http\Controllers\Front\ContactController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 // Route::get('/', [WebsiteController::class, 'index'])->name('home');
 Route::get('/terms', [TermsController::class, 'index'])->name('terms');
 Route::get('/blog', [BlogController::class, 'index'])->name('blog');
+Route::get('contact', [ContactController::class, 'index'])->name('contact');
+Route::post('contact/submit', [ContactController::class, 'submit'])->name('contact_submit');
 
 
 Route::get('/dashboard', [WebsiteController::class, 'dashboard'])->name('dashboard')->middleware('auth');
