4c7c45afe19cf87836267bf39407105d2a023925
Evi Zulfia Rahmah <evizulfia@gmail.com>
2024-06-26 16:59:08 +0700

    fix routes for home update

--- routes/web.php
+++ routes/web.php
@@ -50,6 +50,7 @@ Route::middleware('admin:admin')->group(function (){
     Route::get('/admin/home-page', [AdminHomePageController::class, 'index'])->name('admin_home_page');
     Route::get('/admin/edit-profile', [AdminProfileController::class, 'index'])->name('admin_profile');
     Route::post('/admin/edit-profile-submit', [AdminProfileController::class, 'profile_submit'])->name('admin_profile_submit');
+    Route::post('/admin/home-page/update', [AdminHomePageController::class, 'update'])->name('admin_home_page_update');
 });
 
 
