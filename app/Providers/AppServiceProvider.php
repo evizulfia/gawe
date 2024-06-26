01b248a3d77d4a5ad557611bdad7da9728029f1f
Evi Zulfia Rahmah <evizulfia@gmail.com>
2024-06-27 02:07:39 +0700

    fe login

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,26 +3,25 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
+use App\Models\Banner;
+use App\Models\Setting;
 
 class AppServiceProvider extends ServiceProvider
-{
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
+{  
     public function register()
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
     public function boot()
     {
-        //
+        Paginator::useBootstrap();
+
+        $banner_data = Banner::where('id',1)->first();
+        $settings_data = Setting::where('id',1)->first();
+        view()->share('global_banner_data', $banner_data);
+        view()->share('global_settings_data', $settings_data);
+
     }
 }
