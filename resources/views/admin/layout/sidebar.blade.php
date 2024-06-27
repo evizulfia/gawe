8a1fb17803891d95c1c3de690749555e65a66490
Pandu Sunarya <pandusnrya@gmail.com>
2024-06-27 21:59:12 +0700

    routing why choose us + add CRUD function

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -27,8 +27,7 @@
                 </ul>
             </li>
 
-            {{--<li class=""><a class="nav-link" href="setting.html" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-title="Setting"><i class="fas fa-hand-point-right"></i> <span>Setting</span></a></li>
---}}
+            <li class="{{ Request::is('admin/why-choose/*') ? 'active' : '' }}"><a class="nav-link" href="{{ route('admin_why_choose_item') }}" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-title="Why Choose Items"><i class="fas fa-hand-point-right"></i> <span>Why Choose Items</span></a></li>
             
         </ul>
     </aside>
