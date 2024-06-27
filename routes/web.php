d7af81771ea09b2d976b792e7546c69fa5400744
Evi Zulfia Rahmah <evizulfia@gmail.com>
2024-06-27 19:59:48 +0700

    fix errors with not defined candidate

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ use App\Http\Controllers\Admin\AdminProfileController;
 use App\Http\Controllers\Admin\AdminJobCategoryController;
 use App\Http\Controllers\Admin\AdminOtherPageController;
 
+use App\Http\Controllers\Candidate\CandidateController;
 
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
@@ -85,6 +86,72 @@ Route::middleware('admin:admin')->group(function (){
     Route::get('/admin/job-category', [AdminJobCategoryController::class, 'index'])->name('admin_job_category');
     Route::get('/admin/other-page', [AdminOtherPageController::class, 'index'])->name('admin_other_page');
 });
+
+
+/* Candidate Middleware */
+Route::middleware(['candidate:candidate'])->group(function() {
+    Route::get('/candidate/dashboard', [CandidateController::class, 'dashboard'])->name('candidate_dashboard');
+    Route::get('/candidate/edit-profile', [CandidateController::class, 'edit_profile'])->name('candidate_edit_profile');
+    Route::post('/candidate/edit-profile/update', [CandidateController::class, 'edit_profile_update'])->name('candidate_edit_profile_update');
+    Route::get('/candidate/edit-password', [CandidateController::class, 'edit_password'])->name('candidate_edit_password');
+    Route::post('/candidate/edit-password/update', [CandidateController::class, 'edit_password_update'])->name('candidate_edit_password_update');
+
+    Route::get('/candidate/education/view', [CandidateController::class, 'education'])->name('candidate_education');
+    Route::get('/candidate/education/create', [CandidateController::class, 'education_create'])->name('candidate_education_create');
+    Route::post('/candidate/education/store', [CandidateController::class, 'education_store'])->name('candidate_education_store');
+    Route::get('/candidate/education/edit/{id}', [CandidateController::class, 'education_edit'])->name('candidate_education_edit');
+    Route::post('/candidate/education/update/{id}', [CandidateController::class, 'education_update'])->name('candidate_education_update');
+    Route::get('/candidate/education/delete/{id}', [CandidateController::class, 'education_delete'])->name('candidate_education_delete');
+
+    Route::get('/candidate/skill/view', [CandidateController::class, 'skill'])->name('candidate_skill');
+    Route::get('/candidate/skill/create', [CandidateController::class, 'skill_create'])->name('candidate_skill_create');
+    Route::post('/candidate/skill/store', [CandidateController::class, 'skill_store'])->name('candidate_skill_store');
+    Route::get('/candidate/skill/edit/{id}', [CandidateController::class, 'skill_edit'])->name('candidate_skill_edit');
+    Route::post('/candidate/skill/update/{id}', [CandidateController::class, 'skill_update'])->name('candidate_skill_update');
+    Route::get('/candidate/skill/delete/{id}', [CandidateController::class, 'skill_delete'])->name('candidate_skill_delete');
+
+    Route::get('/candidate/experience/view', [CandidateController::class, 'experience'])->name('candidate_experience');
+    Route::get('/candidate/experience/create', [CandidateController::class, 'experience_create'])->name('candidate_experience_create');
+    Route::post('/candidate/experience/store', [CandidateController::class, 'experience_store'])->name('candidate_experience_store');
+    Route::get('/candidate/experience/edit/{id}', [CandidateController::class, 'experience_edit'])->name('candidate_experience_edit');
+    Route::post('/candidate/experience/update/{id}', [CandidateController::class, 'experience_update'])->name('candidate_experience_update');
+    Route::get('/candidate/experience/delete/{id}', [CandidateController::class, 'experience_delete'])->name('candidate_experience_delete');
+
+    Route::get('/candidate/award/view', [CandidateController::class, 'award'])->name('candidate_award');
+    Route::get('/candidate/award/create', [CandidateController::class, 'award_create'])->name('candidate_award_create');
+    Route::post('/candidate/award/store', [CandidateController::class, 'award_store'])->name('candidate_award_store');
+    Route::get('/candidate/award/edit/{id}', [CandidateController::class, 'award_edit'])->name('candidate_award_edit');
+    Route::post('/candidate/award/update/{id}', [CandidateController::class, 'award_update'])->name('candidate_award_update');
+    Route::get('/candidate/award/delete/{id}', [CandidateController::class, 'award_delete'])->name('candidate_award_delete');
+
+    Route::get('/candidate/resume/view', [CandidateController::class, 'resume'])->name('candidate_resume');
+    Route::get('/candidate/resume/create', [CandidateController::class, 'resume_create'])->name('candidate_resume_create');
+    Route::post('/candidate/resume/store', [CandidateController::class, 'resume_store'])->name('candidate_resume_store');
+    Route::get('/candidate/resume/edit/{id}', [CandidateController::class, 'resume_edit'])->name('candidate_resume_edit');
+    Route::post('/candidate/resume/update/{id}', [CandidateController::class, 'resume_update'])->name('candidate_resume_update');
+    Route::get('/candidate/resume/delete/{id}', [CandidateController::class, 'resume_delete'])->name('candidate_resume_delete');
+
+
+    Route::get('/candidate/bookmark-add/{id}', [CandidateController::class, 'bookmark_add'])->name('candidate_bookmark_add');
+    Route::get('/candidate/bookmark-view', [CandidateController::class, 'bookmark_view'])->name('candidate_bookmark_view');
+    Route::get('/candidate/bookmark-delete/{id}', [CandidateController::class, 'bookmark_delete'])->name('candidate_bookmark_delete');
+
+    Route::get('/candidate/apply/{id}', [CandidateController::class, 'apply'])->name('candidate_apply');
+    Route::post('/candidate/apply-submit/{id}', [CandidateController::class, 'apply_submit'])->name('candidate_apply_submit');
+    Route::get('/candidate/applications', [CandidateController::class, 'applications'])->name('candidate_applications');
+
+});
+
+
+
+
+
+
+
+
+
+
+
 
 
 use Illuminate\Support\Facades\DB;
