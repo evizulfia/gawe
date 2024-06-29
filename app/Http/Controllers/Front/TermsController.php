3a34db01050a1d3662ed15bd122e0d693358e622
Evi Zulfia Rahmah <evizulfia@gmail.com>
2024-06-29 19:52:01 +0700

    fix teremitemcontroller

--- app/Http/Controllers/Front/TermsController.php
+++ app/Http/Controllers/Front/TermsController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 class TermsController extends Controller
 {
     public function index() {
-        return view('front.terms');
+        $term_page_item = PageTermItem::where('id',1)->first();
+        return view('front.terms', compact('term_page_item'));
     }
 }
